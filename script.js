7a00af20d362e72243506ec1866a3b4c8a09cd8e
2DDraghici <draghici.david17@yahoo.com>
2022-12-24 19:30:19 +0200

    added the modal

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function Book(title,author,no_pages,no_pages_read)
     this.no_pages = no_pages
     this.no_pages_read = no_pages_read
 }
+
+const modal_container = document.getElementById("modal_container")
+const add_book = document.getElementById("add_book")
+const close_modal = document.getElementById("close_modal")
+add_book.addEventListener('click', ()=>
+{
+    modal_container.classList.add("modal_show")
+})
+close_modal.addEventListener('click', (e)=>
+{
+    e.preventDefault()
+    modal_container.classList.remove("modal_show")
+})
+
